2b3206391cdb7c8c685de1e550accf9dc75c5396
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-04-23 14:00:16 +0000

    Fix task 0

--- printf.c
+++ printf.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+static con_spe conspe[] = {
+{'c', print_char},
+{'s', print_str},
+{'%', print_percent},
+{'0', NULL}};
+
 /**
  * _printf - produces output according to a format.
  * @format:
@@ -8,14 +14,11 @@
  */
 int _printf(const char *format, ...)
 {
-	con_spe conspe[] = {
-	    {'c', print_char},
-	    {'s', print_str},
-	    {'%', print_percent},
-	    {'0', NULL}};
-
 	va_list ap;
 	unsigned int count = 0, i;
+
+	if (!format || (*format == '%' && *(format + 1) == '\0'))
+		return (1);
 
 	va_start(ap, format);
 
